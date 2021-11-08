9867d2c28456545c218d5e790b273ddd19505b73
sorokapakistanskaya <stellis@yandex.ru>
2021-11-08 17:42:09 +0300

    randoom2

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className='App'>
+      <ul>
+        <li>scc</li>
+        <li>html</li>
+        <li>JavaScript</li>
+        <li>react </li>
+      </ul>
+    </div>
+  );
+}
+export default App;
